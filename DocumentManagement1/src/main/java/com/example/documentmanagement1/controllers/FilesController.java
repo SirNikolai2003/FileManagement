b8402941681452938d95c9b0a6c5457651a6b2bc
zhaduchanhzz <zhaduchanhzz@gmail.com>
2024-07-09 18:25:28 +0700

    upload local file

--- DocumentManagement1/src/main/java/com/example/documentmanagement1/controllers/FilesController.java
+++ DocumentManagement1/src/main/java/com/example/documentmanagement1/controllers/FilesController.java
@@ -2,6 +2,7 @@ package com.example.documentmanagement1.controllers;
 import com.example.documentmanagement1.entities.FileInfo;
 import com.example.documentmanagement1.messages.ResponseMessage;
 import com.example.documentmanagement1.services.FilesStorageService;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -24,7 +25,19 @@ public class FilesController {
     @PostMapping("/upload")
     public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
         String message = "";
+
         try {
+            if (file.isEmpty()) {
+                message = "Please select a file to upload";
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+            }
+
+            // Check file type
+            if (!isValidFileType(file)) {
+                message = "Only XLS, XLSX, or PDF files are allowed";
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+            }
+
             storageService.save(file);
 
             message = "Uploaded the file successfully: " + file.getOriginalFilename();
@@ -54,5 +67,11 @@ public class FilesController {
         Resource file = storageService.load(filename);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+    private boolean isValidFileType(@NotNull MultipartFile file) {
+        String contentType = file.getContentType();
+        return contentType.equals("application/vnd.ms-excel") // XLS
+                || contentType.equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") // XLSX
+                || contentType.equals("application/pdf"); // PDF
     }
 }
